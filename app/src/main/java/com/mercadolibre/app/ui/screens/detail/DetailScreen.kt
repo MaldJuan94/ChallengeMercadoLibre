d5ae610331de659cf391f16769e8accb23d2e124
Juan Guillermo Escorcia Maldonado <juan.escorcia@pragma.com.co>
2022-06-24 07:43:00 -0500

    see product attributes is added

--- app/src/main/java/com/mercadolibre/app/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/mercadolibre/app/ui/screens/detail/DetailScreen.kt
@@ -2,17 +2,18 @@ package com.mercadolibre.app.ui.screens.detail
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -21,11 +22,14 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.lerp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil.compose.rememberAsyncImagePainter
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.mercadolibre.app.R
+import com.mercadolibre.app.models.detail.Attributes
 import com.mercadolibre.app.ui.events.ResultProductState
 import com.mercadolibre.app.ui.screens.error.ErrorScreen
 import com.mercadolibre.app.ui.screens.main.HeaderMain
@@ -66,9 +70,12 @@ fun DetailScreen(
             ) {
                 LoaderSearch(modifier = Modifier.size(100.dp, 16.dp))
             }
-        } else if (state.error.isNullOrEmpty()) {
+        }
+
+        AnimateContent(visible = state.error.isNullOrEmpty() && !state.loading) {
             contentDetail(state)
         }
+
         AnimateContent(visible = state.error != null && !state.loading) {
             ErrorScreen(
                 message = state.error.orEmpty(),
@@ -78,13 +85,20 @@ fun DetailScreen(
     }
 }
 
-@OptIn(ExperimentalPagerApi::class)
+@OptIn(ExperimentalPagerApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun contentDetail(state: ResultProductState) {
     val scrollState = rememberScrollState()
     val itemCondition = remember {
         mutableStateOf(state.data.attributes.find { it.id == "ITEM_CONDITION" }?.valueName)
     }
+
+    val isShowModal = rememberSaveable { mutableStateOf(false) }
+
+    if (isShowModal.value) {
+        AttributesDialog(state.data.attributes) { isShowModal.value = false }
+    }
+
     Column(
         modifier = Modifier
             .verticalScroll(scrollState)
@@ -220,7 +234,7 @@ fun contentDetail(state: ResultProductState) {
         )
 
         Text(
-            text = "Descripción",
+            text = stringResource(id = R.string.product_description),
             style = proximeNovaSemiBold,
             color = GeneralBlack,
             fontSize = 20.sp,
@@ -250,5 +264,109 @@ fun contentDetail(state: ResultProductState) {
                 )
         )
 
+        Text(
+            text = stringResource(id = R.string.product_characteristics),
+            style = proximeNovaSemiBold.copy(
+                textDecoration = TextDecoration.Underline
+            ),
+            color = Blue100,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .clickable {
+                    isShowModal.value = !isShowModal.value
+                }
+                .background(color = GeneralWhite)
+                .fillMaxWidth()
+                .padding(
+                    top = 16.dp,
+                    start = 24.dp,
+                    end = 24.dp,
+                    bottom = 16.dp
+                )
+        )
+    }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun AttributesDialog(
+    attributes: ArrayList<Attributes>,
+    onClickClose: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onClickClose,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false
+        )
+    ) {
+        AttributesDialogBody(
+            attributes = attributes,
+        )
+    }
+}
+
+@Composable
+fun AttributesDialogBody(
+    attributes: ArrayList<Attributes>,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .padding(24.dp, 10.dp, 24.dp, 10.dp)
+            .fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier
+                .background(Color.White)
+        ) {
+            TableScreen(attributes)
+        }
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float,
+    isBold: Boolean = false
+) {
+    Text(
+        text = text,
+        style = if (isBold) proximeNovaBold else proximeNovaRegular,
+        color = GeneralBlack,
+        fontSize = 14.sp,
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+            .weight(weight)
+            .padding(8.dp)
+    )
+}
+
+@Composable
+fun TableScreen(
+    attributes: ArrayList<Attributes>
+) {
+    val column1Weight = .5f
+    val column2Weight = .5f
+    LazyColumn(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        items(attributes.size) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .background(GeneralBackGround)
+            ) {
+                TableCell(text = attributes[it].name ?: "-", weight = column1Weight, isBold = true)
+                TableCell(text = attributes[it].valueName ?: "-", weight = column2Weight)
+            }
+        }
     }
 }
